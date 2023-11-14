e0914e8c9275a67672760e118cfb50b8f0e42496
Femi Gabriel <timfemi@gmail.com>
2023-11-14 15:46:25 +0100

    landing page fixes

--- app/activities/page.tsx
+++ app/activities/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import React from "react";
-import { HomePage } from "@/components/home";
-import Form from "@/components/activities/form";
 import { ActivitiesPage } from "@/components/activities";
 
 export default function Home() {
--- components/home/LandingPage.tsx
+++ components/home/LandingPage.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Header } from "../Header";
 import Image from "next/image";
+import Link from "next/link";
 
 export const LandingPage = () => {
   return (
     <div className="bg-[#EEE0FF] w-full h-screen landingPage pt-32">
-     
       <div className="px-10 pt-10">
         <div className="text-[#9B59B6]">
           <h1 className="text-[1.792em] w-[12em] font-medium leading-[42px] mb-3">
@@ -17,27 +17,34 @@ export const LandingPage = () => {
           </p>
         </div>
         <div className="flex gap-5 mt-20 lists">
-          <Image
-            width={18}
-            height={20}
-            src="./assets/images/Frame3.svg"
-            className="w-[225px] h-[168px] cursor-pointer"
-            alt="card"
-          />
-          <Image
-            width={18}
-            height={20}
-            src="./assets/images/Frame1.svg"
-            className="w-[225px] h-[168px] cursor-pointer"
-            alt="card"
-          />
-          <Image
-            width={18}
-            height={20}
-            src="./assets/images/Frame2.svg"
-            className="w-[225px] h-[168px] cursor-pointer"
-            alt="card"
-          />
+          <Link href="/read-book">
+            <Image
+              width={18}
+              height={20}
+              src="./assets/images/Frame3.svg"
+              className="w-[225px] h-[168px] cursor-pointer"
+              alt="card"
+            />
+          </Link>
+
+          <Link href="/activities">
+            <Image
+              width={18}
+              height={20}
+              src="./assets/images/Frame1.svg"
+              className="w-[225px] h-[168px] cursor-pointer"
+              alt="card"
+            />
+          </Link>
+          <Link href="/">
+            <Image
+              width={18}
+              height={20}
+              src="./assets/images/Frame2.svg"
+              className="w-[225px] h-[168px] cursor-pointer"
+              alt="card"
+            />
+          </Link>
         </div>
       </div>
     </div>
