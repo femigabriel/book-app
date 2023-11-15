189931f01adaacc9185a582d6c13478c27706071
Femi Gabriel <timfemi@gmail.com>
2023-11-15 10:05:26 +0100

    fixes

--- components/activities/bus-stop/BusStop.tsx
+++ components/activities/bus-stop/BusStop.tsx
@@ -10,7 +10,7 @@ export const BusStop = () => {
         <div className="lg:mt-24 mt-10">
           <h1 className="text-[#F6F4F8] text-[1.5em] text-center lg:w-[8em] my-20">BUS STOPS</h1>
           <div className="">
-            <Link href="/activities/charlies-choice" className="bookBtn ">
+            <Link href="/activities/bus-stop" className="bookBtn ">
               <button className="bg-[#9B59B6] h-[47px] text-[#F8F8F8] text-[0.85em] rounded-[24px] flex justify-center items-center w-full lg:w-[202px] my-5">
                 <Image
                   src="/assets/icons/playIcon.svg"
