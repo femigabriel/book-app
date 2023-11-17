1d08665651534dd2677c8d7d1b4ec4a94aa2d5c1
Femi Gabriel <timfemi@gmail.com>
2023-11-17 21:13:08 +0100

    book pages numb

--- components/readBook/book1/Book1Pages.tsx
+++ components/readBook/book1/Book1Pages.tsx
@@ -22,7 +22,7 @@ export const Book1Pages = () => {
             height={300}
             src="/assets/illustrators/book-flip.png"
             className="w-full h-full mb-5 lg:h-[457px] lg:w-[680px] cursor-pointer mt-2"
-            alt="avatar"
+            alt="book-flip"
           />
         </div>
       ),
