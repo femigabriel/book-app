8e5a8680c1a98471583a87a6a88896698db7259f
Femi Gabriel <timfemi@gmail.com>
2024-09-28 10:51:07 +0100

    fixes

--- components/modals/QuitModal.tsx
+++ components/modals/QuitModal.tsx
@@ -1,17 +1,25 @@
 import React, { useState } from "react";
-import { Button, Input, Modal } from "antd";
+import { Modal } from "antd";
 import Image from "next/image";
 import Link from "next/link";
 
 export const QuitModal = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  // Function to play the sound
+  const playSound = () => {
+    const audio = new Audio("/assets/sounds/756267__joseegn__ui_sound_return_1.wav"); 
+    audio.play();
+  };
+
   const showModal = () => {
     setIsModalOpen(true);
+    
   };
 
   const handleOk = () => {
     setIsModalOpen(false);
+    playSound(); 
   };
 
   const handleCancel = () => {
@@ -44,11 +52,10 @@ export const QuitModal = () => {
                 height={19}
                 src="/assets/icons/head.svg"
                 className="w-[70px] h-[70px] cursor-pointer"
-                alt="back-icon"
-                onClick={showModal}
+                alt="head-icon"
               />
             </div>
-            <h1 className="text-[2em]"> Do you really want to quit?</h1>
+            <h1 className="text-[2em]">Do you really want to quit?</h1>
 
             <div className="w-full flex justify-between py-5 pt-20">
               <Link href="/activities/activities-card">
@@ -73,3 +80,82 @@ export const QuitModal = () => {
     </div>
   );
 };
+
+
+
+
+// import React, { useState } from "react";
+// import { Button, Input, Modal } from "antd";
+// import Image from "next/image";
+// import Link from "next/link";
+
+// export const QuitModal = () => {
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+
+//   const showModal = () => {
+//     setIsModalOpen(true);
+//   };
+
+//   const handleOk = () => {
+//     setIsModalOpen(false);
+//   };
+
+//   const handleCancel = () => {
+//     setIsModalOpen(false);
+//   };
+
+//   return (
+//     <div className="w-full">
+//       <div>
+//         <Image
+//           width={17}
+//           height={19}
+//           src="/assets/icons/quitIcon.svg"
+//           className="w-[68px] h-[25px] cursor-pointer"
+//           alt="back-icon"
+//           onClick={showModal}
+//         />
+//       </div>
+//       <Modal
+//         footer={null}
+//         open={isModalOpen}
+//         onOk={handleOk}
+//         onCancel={handleCancel}
+//       >
+//         <div className="flex justify-center">
+//           <div className="mt-5">
+//             <div className="flex justify-center items-center">
+//               <Image
+//                 width={17}
+//                 height={19}
+//                 src="/assets/icons/head.svg"
+//                 className="w-[70px] h-[70px] cursor-pointer"
+//                 alt="back-icon"
+//                 onClick={showModal}
+//               />
+//             </div>
+//             <h1 className="text-[2em]"> Do you really want to quit?</h1>
+
+//             <div className="w-full flex justify-between py-5 pt-20">
+//               <Link href="/activities/activities-card">
+//                 <button
+//                   className="bg-[#9B59B6] rounded-[24px] lg:h-[47px] px-5 lg:px-16 text-white text-[1em]"
+//                   onClick={handleOk}
+//                 >
+//                   Yes
+//                 </button>
+//               </Link>
+
+//               <button
+//                 className="bg-[#AED5F2] rounded-[24px] lg:h-[47px] px-5 lg:px-16 text-white text-[1em]"
+//                 onClick={handleCancel}
+//               >
+//                 No
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       </Modal>
+//     </div>
+//   );
+// };
